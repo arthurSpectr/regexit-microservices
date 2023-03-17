14ab542dd8f4b312112b55cce7dee71cc5a9cbb7
arturtyan <artur.tyan93@gmail.com>
2023-03-17 12:06:49 -0400

    add connecting to db

---
+++ customer/src/main/java/regexit/customer/CustomerRegistrationRequest.java
@@ -0,0 +1,7 @@
+package regexit.customer;
+
+public record CustomerRegistrationRequest(
+        String firstName,
+        String lastName,
+        String email) {
+}
---
+++ customer/src/main/java/regexit/customer/CustomerService.java
@@ -0,0 +1,19 @@
+package regexit.customer;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public record CustomerService(CustomerRepository customerRepository) {
+    public void registerCustomer(CustomerRegistrationRequest request) {
+        Customer customer = Customer.builder()
+                .firstname(request.firstName())
+                .lastname(request.lastName())
+                .email(request.email())
+                .build();
+
+        //todo: check if email valid
+        //todo: check if email not taken
+        //todo: store customer in db
+        customerRepository.save(customer);
+    }
+}
